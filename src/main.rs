a2d79206493242f0846097e4bcf63223c96e39cf
Happya <axlu810+github@gmail.com>
2025-10-01 17:55:48 +0000

    made mutation file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use rand::prelude::*;
+use ndarray::{Array1, Array2};
+use quizx::graph::{GraphLike, Graph};
+use quizx::vec_graph::VecGraph;
 use quizx::basic_rules::*;
+use quizx::traits::*;
+use quizx::graph::VertexType;
+use quizx::graph::EdgeType;
 
 fn main() {
     println!("asdfasdf");
-    println!("why are things not saving");
 }
