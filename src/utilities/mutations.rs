86b1bafefb68244515cf3de77064a8a246ae407e
Happya <axlu810+github@gmail.com>
2025-10-05 16:19:39 -0400

    added mutations

---
+++ src/utilities/mutations.rs
@@ -0,0 +1,10 @@
+use quizx::circuit::*;
+use quizx::extract::*;
+use quizx::simplify::*;
+use quizx::vec_graph::*;
+
+pub fn complement(graph : Graph, vertex : u32) {
+
+  let neighbors = graph.neighbors(vertex);
+
+}
