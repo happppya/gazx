75afd5a363e4db6b2dd3ebe7da95fa470cba6eeb
Happya <axlu810+github@gmail.com>
2025-10-05 16:45:29 -0400

    started on complement mutation

--- src/utilities/mutations.rs
+++ src/utilities/mutations.rs
@@ -3,8 +3,28 @@ use quizx::extract::*;
 use quizx::simplify::*;
 use quizx::vec_graph::*;
 
-pub fn complement(graph : Graph, vertex : u32) {
+pub fn complement(graph : Graph, vertex : usize) {
 
-  let neighbors = graph.neighbors(vertex);
+  let neighbors : Vec<usize> = graph.neighbors(vertex).collect();
+  let mut done_neighbors : Vec<usize> = Vec::new();
+
+  let edges = graph.edges();
+  
+  for (node, idk, asdf) in edges {
+    println!("{:?}, {:?}, {:?}", node, idk, asdf);
+  }
+
+  for &n1 in &neighbors {
+    done_neighbors.push(n1);
+
+    for &n2 in &neighbors {
+
+      if (done_neighbors.contains(&n2)) {
+        continue;
+      }
+
+    }
+
+  }
 
 }
