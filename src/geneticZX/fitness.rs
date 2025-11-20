66ccf7c9ae1bd57ddc494ab06a2f80079faf51d9
Happya <axlu810+github@gmail.com>
2025-11-20 14:48:12 +0000

    Trash

--- src/geneticZX/fitness.rs
+++ src/geneticZX/fitness.rs
@@ -1,11 +1,22 @@
 use quizx::circuit::{Circuit, CircuitStats};
+use std::sync::LazyLock;
+
+static GOAL_CIRCUIT: LazyLock<Circuit> =
+    LazyLock::new(|| Circuit::from_file("circuits/small/grover_5.qasm")?.to_basic_gates());
+
+static GOAL_STATS: LazyLock<CircuitStats> = LazyLock::new(|| GOAL_CIRCUIT.stats());
 
 fn get_component_approximation_error(circuit: Circuit) {}
 
-fn get_component_depth(circuit: Circuit) {
+fn get_component_depth(circuit: Circuit) -> i64 {
     let stats = circuit.stats();
 }
 
-fn get_component_complex_gates() {}
+fn get_component_complex_gates(circuit: Circuit) -> i64 {
+    let stats = circuit.stats();
+    let two_qubit_diff = GOAL_STATS.twoq - stats.twoq;
+
+    return two_qubit_diff as i64;
+}
 
 fn get_component_input_encodings() {}
