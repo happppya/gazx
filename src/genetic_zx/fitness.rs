c82e7945d15f655c4f2cfba9ba477efd921d824f
Happya <axlu810+github@gmail.com>
2025-12-01 17:28:11 -0500

    updated fitness functions

--- src/genetic_zx/fitness.rs
+++ src/genetic_zx/fitness.rs
@@ -1,52 +1,46 @@
-use quizx::{
-    circuit::{Circuit, CircuitStats},
-    graph::GraphLike,
-    vec_graph::Graph,
-};
+use quizx::{circuit::{Circuit, CircuitStats}, graph::GraphLike, vec_graph::Graph};
 use std::sync::LazyLock;
 
-use super::models::GraphPopulation;
-
-static GOAL_CIRCUIT: LazyLock<Circuit> = LazyLock::new(|| {
-    Circuit::from_file("circuits/small/grover_5.qasm")
-        .unwrap()
-        .to_basic_gates()
+static GOAL_CIRCUIT : LazyLock<Circuit> = LazyLock::new(|| {
+    Circuit::from_file("circuits/small/grover_5.qasm").unwrap().to_basic_gates()
 });
 
-static GOAL_CIRCUIT_STATS: LazyLock<CircuitStats> = LazyLock::new(|| GOAL_CIRCUIT.stats());
+static GOAL_GRAPH : LazyLock<Graph> = LazyLock::new(|| {
+    GOAL_CIRCUIT.to_graph_with_options(true, true)
+});
 
-fn get_component_approximation_error(graph: &Graph, circuit: &Circuit) -> i64 {
+static GOAL_CIRCUIT_STATS : LazyLock<CircuitStats> = LazyLock::new(|| {
+    GOAL_CIRCUIT.stats()
+});
+
+fn get_approximation_error(graph : &Graph, circuit : &Circuit) -> i64 {
     0
 }
 
-fn get_component_depth(graph: &Graph, circuit: &Circuit) -> i64 {
-    0
+fn get_depth(graph : &Graph, _circuit : &Circuit) -> i64 {
+    (graph.depth() - GOAL_GRAPH.depth()) as i64
 }
 
-fn get_component_complex_gates(graph: &Graph, circuit: &Circuit) -> i64 {
+fn get_complex_gates(_graph : &Graph, circuit : &Circuit) -> i64 {
     let stats = circuit.stats();
-    let two_qubit_diff = stats.twoq as i64 - GOAL_CIRCUIT_STATS.twoq as i64;
-    return two_qubit_diff;
+    let two_qubit_diff = stats.twoq - GOAL_CIRCUIT_STATS.twoq;
+    return two_qubit_diff as i64
 }
 
-fn get_component_input_encodings(graph: &Graph, circuit: &Circuit) -> i64 {
-    -1 * graph.inputs().len() as i64
+fn get_input_encodings(graph : &Graph, _circuit : &Circuit) -> i64 {
+    (graph.inputs().len() - GOAL_GRAPH.inputs().len()) as i64
 }
 
-fn get_fitness(graph: &Graph, circuit: &Circuit) -> i64 {
-    let component_approximation_error = get_component_approximation_error(graph, circuit);
-    let component_depth = get_component_depth(graph, circuit);
-    let component_complex_gates = get_component_complex_gates(graph, circuit);
-    let component_input_encodings = get_component_input_encodings(graph, circuit);
+pub fn get_fitness(graph : Graph, circuit : Circuit) -> i64 {
 
-    return 1 * component_approximation_error
-        + 1 * component_depth
-        + 1 * component_complex_gates
-        + 1 * component_input_encodings;
-}
+    let approximation_error = get_approximation_error(&graph, &circuit);
+    let depth = get_depth(&graph, &circuit);
+    let complex_gates = get_complex_gates(&graph, &circuit);
+    let input_encodings = get_input_encodings(&graph, &circuit);
 
-pub fn set_fitness_values(population: &mut GraphPopulation) {
-    for i in 0..population.graphs.len() {
-        population.fitness_values[i] = get_fitness(&population.graphs[i], &population.circuits[i]);
-    }
+    return 
+        0 * approximation_error + // unimplemented
+        -10 * depth +
+        -10 * complex_gates + 
+        -10 * input_encodings;
 }
